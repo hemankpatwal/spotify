a2908d2a6a8ccd2313bfc61ae0d66640609b6ea2
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2022-02-25 23:52:41 +0530

    logo

---
+++ src/components/Landing.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Landing = () => (
+    <div className="w-full h-full">
+        Hello
+    </div>    
+)   
+
+export default Landing
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+import Landing from '../components/Landing'
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-full h-full">
+     
+     <Landing/>
+    </div>
+  )
+}
+
+export default Home
