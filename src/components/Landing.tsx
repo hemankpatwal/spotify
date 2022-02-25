359c322833aa283e1dcd62e9cf712eb56d519c2e
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2022-02-26 03:04:53 +0530

    left column

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,7 +2,92 @@ import React from "react"
 
 const Landing = () => (
     <div className="w-full h-full">
-        Hello
+        <div>
+            <div className="flex flex-col bg-black w-[15%]">
+                <div className="ml-[10%] mt-[15%]">
+                    <img src="/images/icon1.png"/>
+                </div>
+                <div className="space-y-2">
+                    <div className="flex mt-[20%] justify-start space-x-5">
+                        <div>
+                            <img src="/images/icon3.png" />
+                        </div>
+                        <div className="mt-[2%]">
+                            <img src="/images/icon2.png"/>
+                        </div>
+                        <div className=" text-white text-xl">
+                            Home
+                        </div>
+                    </div>
+                    <div className="flex mt-[20%] justify-start space-x-5 ml-[8%]">
+                        <div className="mt-[2%]">
+                            <img src="/images/icon4.png"/>
+                        </div>
+                        <div className=" text-white text-xl">
+                            Search
+                        </div>
+                    </div>
+                    <div className="flex mt-[20%] justify-start space-x-5 ml-[8%]">
+                        <div className="mt-[2%]">
+                            <img src="/images/icon5.png"/>
+                        </div>
+                        <div className=" text-white text-xl">
+                            Your Library
+                        </div>
+                    </div>
+                   
+                </div>
+                <div className=" text-white mt-[45%] ml-[8%] space-y-5 ">
+                    <div className="text-xl ">
+                        Playlists
+                    </div>                 
+                    <div className=" flex space-x-5 ">
+                        
+                        <div className="">
+                            <img src="/images/icon7.png" />
+                        </div>
+                        <div className="font-semibold text-lg">
+                            Create new playlist
+                        </div>
+                    </div>
+                    <div className="font-medium text-base">
+                            Dope Shope - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            Brown Rang - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            Blue Eyes - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            Manali Trance - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            High Heels - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            Chaar Bottle Vodka - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            Sunny Sunny - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            Breakup Party - Honey Singh
+                    </div>
+                    <div className="font-medium text-base">
+                            Party All Night - Honey Singh
+                    </div>
+                    
+                </div>
+            </div>
+
+
+
+            <div>
+
+            </div>
+        </div>
+        
     </div>    
 )   
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 
 import Landing from '../components/Landing'
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
     <div className="w-full h-full">
      
      <Landing/>
+     {/* <Footer/> */}
     </div>
   )
 }
